60726ab2ccaa4f77a8b8c595ed4450e9aa40a148
Jonathan Joel Anchundia Navas <joel.an_92@hotmail.com.ar>
2022-12-24 10:18:49 -0500

    Se hace proceso para manejar el boton "Lanzar bolilla" con JS, mas no desde el controller. Se crean metodos de búsquedas para evitar números repetidos y solo mostrar los que no se han repetidos.

--- BingoWeb/Controllers/HomeController.cs
+++ BingoWeb/Controllers/HomeController.cs
@@ -25,169 +25,13 @@ namespace BingoWeb.Controllers
         }
 
         public IActionResult Index()
-        {
-            //Nueva instancia de random(Valores aleatorios).
-            var genRandom = new Random();
-            var carton = new int[3, 9];
-
-            //generamos 4 cartones
-            for (int i = 0; i < 4; i++)
-            {
-                //Recorremos columnas x filas
-                //Generamos números aleatorios para el cartón.
-                for (int c = 0; c < 9; c++)
-                {
-                    for (int f = 0; f < 3; f++)
-                    {
-                        int nuevoNumero = 0;
-                        bool encontreUnoNuevo = false;
-
-                        while (!encontreUnoNuevo)
-                        {
-                            if (c == 0) //columna 1
-                            {
-                                nuevoNumero = genRandom.Next(1, 10);//Columna 1 Generamos números del 1 al 9
-                            }
-                            else if (c == 8)
-                            { //columna 9
-                                nuevoNumero = genRandom.Next(80, 91);//Columna 9 Generamos números del 80 al 90
-                            }
-                            else//todas las demas columnas
-                            {
-                                nuevoNumero = genRandom.Next(c * 10, c * 10 + 10);
-                            }
-
-                            //Buscamos si el nuevo número generado existe en la columna
-                            encontreUnoNuevo = true;
-                            for (int f2 = 0; f2 < 3; f2++)
-                            {
-                                if (carton[f2, c] == nuevoNumero)
-                                {
-                                    encontreUnoNuevo = false;
-                                    break;
-                                }
-                            }
-                            //Si salio del bucle y no encontró repetidos,
-                            //encontreUnoNuevo = false y sale del bucle while
-                        }
-                        carton[f, c] = nuevoNumero;
-                    }
-                }
-
-                //Ordenamos las columnas
-                for (int c = 0; c < 9; c++)
-                {
-                    for (int f = 0; f < 3; f++)
-                    {
-                        for (int k = f + 1; k < 3; k++)
-                        {
-                            if (carton[f, c] > carton[k, c])
-                            {
-                                int aux = carton[f, c];
-                                carton[f, c] = carton[k, c];
-                                carton[k, c] = aux;
-                            }
-                        }
-                    }
-                }
-
-                var borrados = 0;
-                while (borrados < 12)
-                {
-                    var filaBorrar = genRandom.Next(0, 3);
-                    var columnaBorrar = genRandom.Next(0, 9);
-
-                    if (carton[filaBorrar, columnaBorrar] == 0)
-                        continue;
-
-                    //Contamos cuantos ceros hay en esta fila.
-                    var cerosEnFila = 0;
-                    for (int c = 0; c < 9; c++)
-                    {
-                        if (carton[filaBorrar, c] == 0)
-                        {
-                            cerosEnFila++;
-                        }
-                    }
-
-                    //Contamos cuantos ceros hay en esta columna.
-                    var cerosEnColumna = 0;
-                    for (int f = 0; f < 3; f++)
-                    {
-                        if (carton[f, columnaBorrar] == 0)
-                        {
-                            cerosEnColumna++;
-                        }
-                    }
-
-                    //Contamos cuantos items tenemos
-                    var itemsPorColumna = new int[9];
-                    for (int c = 0; c < 9; c++)
-                    {
-                        for (int f = 0; f < 3; f++)
-                        {
-                            if (carton[f, c] != 0)
-                            {
-                                itemsPorColumna[c]++;
-                            }
-                        }
-                    }
-
-                    //Contamos cuantas columnas hay con un solo número
-                    var columnasConUnNumero = 0;
-                    for (int c = 0; c < 9; c++)
-                    {
-                        if (itemsPorColumna[c] == 1)
-                        {
-                            columnasConUnNumero++;
-                        }
-                    }
-
-                    //Si ya hay 4 ceros en la fila o
-                    //si ya hay 2 ceros en la columna no se hace nada
-                    if (cerosEnFila == 4 || cerosEnColumna == 2) continue;
-
-                    //Si hay 3 columnas con 1 solo número, a partir de ahora
-                    //debo borrar solo las columnas q tienen 3 items
-                    if (columnasConUnNumero == 3 && itemsPorColumna[columnaBorrar] != 3)
-                    {
-                        continue;
-                    }
-
-                    //Si no entro por las opciones anteriores borramos el número
-                    carton[filaBorrar, columnaBorrar] = 0;
-                    borrados++;
-                }
-
-                //Mostramos el cartón
-                Console.WriteLine("----------------------------------------------");
-                for (int f = 0; f < 3; f++)
-                {
-                    for (int c = 0; c < 9; c++)
-                    {
-                        if (c == 0)
-                            Console.Write("|");
-
-                        //Si es 0 mostramos espacios
-                        if (carton[f, c] == 0)
-                            Console.Write("    |");
-                        else
-                            Console.Write($" {carton[f, c]:00} |");
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("----------------------------------------------");
-            }
-
-            ViewBag.test = carton;
-
+        {           
             return View();
         }
 
         [HttpGet]
         public IActionResult ConsultaDatos()
         {
-
             //Nueva instancia de random(Valores aleatorios).
             var genRandom = new Random();
             int[,]? carton = new int[3, 9];
@@ -360,11 +204,12 @@ namespace BingoWeb.Controllers
             return Json(new { carton2=ss, dataBingoL = dataBingoL });
         }
 
-        [HttpGet]
+        [HttpPost]
         public IActionResult TirarBola()
         {
             var genRandom = new Random();
-            int  nuevoNumero = genRandom.Next(1, 90);//Columna 1 Generamos números del 1 al 9
+            int  nuevoNumero = genRandom.Next(1, 90);//Columna 1 Generamos números del 1 al 90
+
             return Json(new { bola = nuevoNumero});
         }
 
